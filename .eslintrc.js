e825731c6ea6889cdb031b86d2bda382c234d81f
Thiago Gomes <thiagosiqgomes@gmail.com>
2024-01-16 15:20:04 -0300

    feat: set up basic monorepo structure

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    root: true,
+    extends: ['@smooth-bookish/eslint-config-custom'],
+};
---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: ["next", "turbo", "prettier"],
+    rules: {
+        "@next/next/no-html-link-for-pages": "off",
+        'no-console': 'error',
+    },
+    parserOptions: {
+        babelOptions: {
+            presets: [require.resolve("next/babel")],
+        },
+    },
+};
+
+
